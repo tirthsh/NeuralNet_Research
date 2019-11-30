a667296d310a53edd593b0650da751d9f67415cc
Tirth Shah <tirth96@hotmail.com>
2019-11-29 23:32:19 -0500

    model performing a little better on validation set (good bc means its not memorizing)
    training_loss: 0.2754 - training_acc: 0.8834 - val_loss: 0.2268 - val_acc: 0.9122

--- vizwiz_cnn.py
+++ vizwiz_cnn.py
@@ -58,8 +58,7 @@ for feature, label in training_data:
 x_train, x_test, y_train, y_test = train_test_split(feature_set, label_set, test_size = 0.2)
 
 #classification problem -> [0,1] => [non-priv, priv] 
-classes = np.unique(np.array(y_train))
-nclasses = len(classes)
+num_classes = len(np.unique(np.array(y_train)))
 
 #normalize between 0 - 1
 x_train = x_train / 255.0
@@ -75,24 +74,24 @@ x_train,x_valid,train_label,valid_label = train_test_split(x_train, y_train_one_
 #begin forming model
 vizwiz_model = Sequential()
 
-vizwiz_model.add(Conv2D(32, (3, 3),input_shape=(IMG_SIZE,IMG_SIZE,1)))
-vizwiz_model.add(MaxPooling2D(pool_size=(2, 2)))
-vizwiz_model.add(Dropout(0.25))
+vizwiz_model.add(Conv2D(32, (3, 3),input_shape=(IMG_SIZE,IMG_SIZE,1), padding = "same"))
+vizwiz_model.add(MaxPooling2D(pool_size=(2, 2), padding = "same"))
+vizwiz_model.add(Dropout(0.2))
 vizwiz_model.add(Activation("relu"))
 
-vizwiz_model.add(Conv2D(64, (3, 3)))
-vizwiz_model.add(MaxPooling2D(pool_size=(2, 2)))
-vizwiz_model.add(Dropout(0.25))
+vizwiz_model.add(Conv2D(64, (3, 3), padding = "same"))
+vizwiz_model.add(MaxPooling2D(pool_size=(2, 2), padding = "same"))
+vizwiz_model.add(Dropout(0.3))
 vizwiz_model.add(Activation("relu"))
 
-vizwiz_model.add(Conv2D(64, (3, 3)))
-vizwiz_model.add(MaxPooling2D(pool_size=(2, 2)))
-vizwiz_model.add(Dropout(0.4))
+vizwiz_model.add(Conv2D(64, (3, 3), padding = "same"))
+vizwiz_model.add(MaxPooling2D(pool_size=(2, 2), padding = "same"))
+vizwiz_model.add(Dropout(0.3))
 vizwiz_model.add(Activation("relu"))
 
-vizwiz_model.add(Conv2D(64, (3, 3)))
-vizwiz_model.add(MaxPooling2D(pool_size=(2, 2)))
-vizwiz_model.add(Dropout(0.4))
+vizwiz_model.add(Conv2D(64, (3, 3), padding = "same"))
+vizwiz_model.add(MaxPooling2D(pool_size=(2, 2), padding = "same"))
+vizwiz_model.add(Dropout(0.3))
 vizwiz_model.add(Activation("relu"))
 
 vizwiz_model.add(Flatten())
@@ -100,20 +99,19 @@ vizwiz_model.add(Flatten())
 vizwiz_model.add(Dense(128, activation='linear'))
 vizwiz_model.add(Activation("relu"))
 
-vizwiz_model.add(Dropout(0.3))
 vizwiz_model.add(Dense(num_classes, activation='sigmoid'))
 
+#define loss function (binary bc only 2 classes), optimzer (adam is usually the most used) 
+#and what you want the model to measure
 vizwiz_model.compile(loss="binary_crossentropy", 
              optimizer="adam",
              metrics=['accuracy'])
 
 vizwiz_model.summary()
 
-
 vizwiz_model.fit(x_train, train_label, batch_size=batch_size,epochs=epochs,validation_data=(x_valid, valid_label))
 
-#test your results with test data
-#test your results with test data
+#test our model results with test data
 predicted_classes = vizwiz_model.predict(x_test)
 predicted_classes = np.argmax(np.round(predicted_classes),axis=1)
 
